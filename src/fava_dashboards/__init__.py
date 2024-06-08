72890f62734ffc4b44eb49637bc67651f188191d
Andreas Gerstmayr <andreas@gerstmayr.me>
2024-06-09 00:09:08 +0200

    fix possible NaNs in panels

--- src/fava_dashboards/__init__.py
+++ src/fava_dashboards/__init__.py
@@ -117,6 +117,14 @@ class FavaDashboards(FavaExtensionBase):
     def bootstrap(self, dashboard_id):
         ext_config = self.read_ext_config()
         operating_currencies = self.ledger.options["operating_currency"]
+
+        if len(operating_currencies) == 0:
+            raise FavaAPIError(f"no operating currency specified in the ledger")
+        if not g.filtered._date_first or not g.filtered._date_last:
+            raise FavaAPIError(
+                f"cannot determine first/last day of ledger, is the ledger empty?"
+            )
+
         commodities = {c.currency: c for c in self.ledger.all_entries_by_type.Commodity}
         accounts = self.ledger.accounts
         ledger = {
